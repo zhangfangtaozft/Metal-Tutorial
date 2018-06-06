399afcd0fd4fc17c81a0e18a8cf7bcb5344ae5ec
Frank.Zhang <Frank.Zhang@neulion.com.cn>
2018-06-06 17:43:38 +0800

    add Ch02-TvOS

---
+++ Ch02-TvOS/Ch02-TvOS/MetalView.swift
@@ -0,0 +1,28 @@
+//
+//  MetalView.swift
+//  Ch02-TvOS
+//
+//  Created by frank.Zhang on 06/06/2018.
+//  Copyright © 2018 Frank.Zhang. All rights reserved.
+//
+
+import MetalKit
+
+class MetalView: MTKView {
+    required init(coder:NSCoder){
+        super.init(coder:coder)
+        device = MTLCreateSystemDefaultDevice()
+    }
+    override func draw(_ rect: CGRect) {
+        if let drawable = currentDrawable,
+            let rpd = currentRenderPassDescriptor{
+            rpd.colorAttachments[0].clearColor = MTLClearColor(red: 0, green: 0.5, blue: 0.5, alpha: 1.0)
+            rpd.colorAttachments[0].loadAction = .clear
+            let commandBuffer = device!.makeCommandQueue()?.makeCommandBuffer()
+            let commandEncoder = commandBuffer?.makeRenderCommandEncoder(descriptor: rpd)
+            commandEncoder?.endEncoding()
+            commandBuffer?.present(drawable)
+            commandBuffer?.commit()
+        }
+    }
+}
---
+++ Ch02-TvOS/Ch02-TvOS/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Ch02-TvOS
+//
+//  Created by frank.Zhang on 06/06/2018.
+//  Copyright © 2018 Frank.Zhang. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
