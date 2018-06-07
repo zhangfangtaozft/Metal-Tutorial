f2b67c6cb67792635fd714716a026a90e8521871
Frank.Zhang <Frank.Zhang@neulion.com.cn>
2018-06-07 16:19:31 +0800

    add Ch04-OSX

---
+++ Ch04-OSX/Ch04-OSX/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  Ch04-OSX
+//
+//  Created by frank.Zhang on 07/06/2018.
+//  Copyright © 2018 Frank.Zhang. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
