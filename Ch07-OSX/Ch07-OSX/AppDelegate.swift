71f2abfefdcb87c18ec6e5edb1952edf6435641b
Frank.Zhang <Frank.Zhang@neulion.com.cn>
2018-06-12 17:24:58 +0800

    add Ch07-OSX

---
+++ Ch07-OSX/Ch07-OSX/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  Ch07-OSX
+//
+//  Created by frank.Zhang on 12/06/2018.
+//  Copyright © 2018 Frank.Zhang. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ Ch07-OSX/Ch07-OSX/MetalView.swift
@@ -0,0 +1,67 @@
+//
+//  MetalView.swift
+//  Ch07-OSX
+//
+//  Created by frank.Zhang on 12/06/2018.
+//  Copyright © 2018 Frank.Zhang. All rights reserved.
+//
+
+import MetalKit
+
+class MetalView: MTKView {
+
+    var commandQueue: MTLCommandQueue?
+    var rps: MTLRenderPipelineState?
+    var vertexBuffer: MTLBuffer!
+    var uniformBuffer:MTLBuffer!
+    
+    required init(coder: NSCoder){
+        super.init(coder:coder)
+        createBuffers()
+        registerShaders()
+    }
+    
+    func createBuffers(){
+        device = MTLCreateSystemDefaultDevice()
+        commandQueue = device?.makeCommandQueue()
+        let vertex_data = [Vertex(position: [-1.0, -1.0, 0.0, 1.0], color: [1, 0, 0, 1]),
+                           Vertex(position: [ 1.0, -1.0, 0.0, 1.0], color: [0, 1, 0, 1]),
+                           Vertex(position: [ 0.0,  1.0, 0.0, 1.0], color: [0, 0, 1, 1])
+        ]
+        vertexBuffer = device?.makeBuffer(bytes: vertex_data, length: MemoryLayout<Vertex>.size * 3, options: [])
+        uniformBuffer = device?.makeBuffer(length: MemoryLayout<Float>.size * 16, options: [])
+        let bufferPointer = uniformBuffer.contents()
+        memcpy(bufferPointer, Matrix().modelMatrix(Matrix()).m, MemoryLayout<Float>.size * 16)
+    }
+    
+    func registerShaders(){
+        let library = device?.makeDefaultLibrary()!
+        let vertex_func = library?.makeFunction(name: "vertex_func")
+        let frag_func = library?.makeFunction(name: "fragment_func")
+        let rpld = MTLRenderPipelineDescriptor()
+        rpld.vertexFunction = vertex_func
+        rpld.fragmentFunction = frag_func
+        rpld.colorAttachments[0].pixelFormat = .bgra8Unorm
+        do{
+            try rps = device?.makeRenderPipelineState(descriptor: rpld)
+        } catch let error {
+            self.printView("\(error)")
+        }
+    }
+    
+    override func draw(_ dirtyRect: NSRect) {
+        super.draw(dirtyRect)
+        if let rpd = currentRenderPassDescriptor, let drawable = currentDrawable {
+            rpd.colorAttachments[0].clearColor = MTLClearColorMake(0.5, 0.5, 0.5, 1.0)
+            let commandBuffer = device?.makeCommandQueue()?.makeCommandBuffer()
+            let commandEncoder = commandBuffer?.makeRenderCommandEncoder(descriptor: rpd)
+            commandEncoder?.setRenderPipelineState(rps!)
+            commandEncoder?.setVertexBuffer(vertexBuffer, offset: 0, index: 0)
+            commandEncoder?.setVertexBuffer(uniformBuffer, offset: 0, index: 1)
+            commandEncoder?.drawPrimitives(type: .triangle, vertexStart: 0, vertexCount: 3, instanceCount: 1)
+            commandEncoder?.endEncoding()
+            commandBuffer?.present(drawable)
+            commandBuffer?.commit()
+        }
+    }
+}
