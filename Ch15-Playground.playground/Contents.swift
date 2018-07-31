d34ba8453b0fbd9db2dd27c1c1db1db96a38d6a8
Frank.Zhang <Frank.Zhang@neulion.com.cn>
2018-07-31 15:43:20 +0800

    Add Ch15-Playground

---
+++ Ch15-Playground.playground/Contents.swift
@@ -0,0 +1,9 @@
+//: Playground - noun: a place where people can play
+
+import Cocoa
+import PlaygroundSupport
+
+let device = MTLCreateSystemDefaultDevice()!
+let frame = NSRect(x:0, y:0, width:400, height:400)
+let view = MetalView(frame: frame, device: device)
+PlaygroundPage.current.liveView = view
---
+++ Ch15-Playground.playground/Sources/MetalView.swift
@@ -0,0 +1,56 @@
+import MetalKit
+
+public class MetalView: MTKView, NSWindowDelegate {
+    
+    var queue: MTLCommandQueue! = nil
+    var cps: MTLComputePipelineState! = nil
+    var timer: Float = 0
+    var timerBuffer: MTLBuffer!
+    
+    required public init(coder: NSCoder) {
+        super.init(coder: coder)
+    }
+    
+    override public init(frame frameRect: CGRect, device: MTLDevice?) {
+        super.init(frame: frameRect, device: device)
+        registerShaders()
+    }
+    
+    override public func draw(_ dirtyRect: NSRect) {
+        if let drawable = currentDrawable,
+            let commandBuffer = queue.makeCommandBuffer(),
+            let commandEncoder = commandBuffer.makeComputeCommandEncoder() {
+            commandEncoder.setComputePipelineState(cps)
+            commandEncoder.setTexture(drawable.texture, index: 0)
+            commandEncoder.setBuffer(timerBuffer, offset: 0, index: 0)
+            update()
+            let threadGroupCount = MTLSizeMake(8, 8, 1)
+            let threadGroups = MTLSizeMake(drawable.texture.width / threadGroupCount.width, drawable.texture.height / threadGroupCount.height, 1)
+            commandEncoder.dispatchThreadgroups(threadGroups, threadsPerThreadgroup: threadGroupCount)
+            commandEncoder.endEncoding()
+            commandBuffer.present(drawable)
+            commandBuffer.commit()
+        }
+        
+    }
+    
+    func update() {
+        timer += 0.01
+        let bufferPointer = timerBuffer.contents()
+        memcpy(bufferPointer, &timer, MemoryLayout<Float>.size)
+    }
+    
+    func registerShaders() {
+        queue = device!.makeCommandQueue()
+        let path = Bundle.main.path(forResource: "Shaders", ofType: "metal")
+        do {
+            let input = try String(contentsOfFile: path!, encoding: String.Encoding.utf8)
+            let library = try device!.makeLibrary(source: input, options: nil)
+            let kernel = library.makeFunction(name: "compute")!
+            cps = try device!.makeComputePipelineState(function: kernel)
+        } catch let e {
+            Swift.print("\(e)")
+        }
+        timerBuffer = device!.makeBuffer(length: MemoryLayout<Float>.size, options: [])
+    }
+}
