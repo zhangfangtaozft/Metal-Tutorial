c9ba2809bd0434ee9096c16ddac9613187e1b1cb
Frank.Zhang <Frank.Zhang@neulion.com.cn>
2018-08-03 16:57:51 +0800

    Add Ch16-Playground

---
+++ Ch16-Playgrond.playground/Contents.swift
@@ -0,0 +1,10 @@
+//: Playground - noun: a place where people can play
+
+import MetalKit
+import PlaygroundSupport
+
+let view = MTKView(frame: NSRect(x: 0, y: 0, width: 300, height: 300))
+let renderer = Render(mtkView: view)
+view.delegate = renderer
+PlaygroundPage.current.liveView = view
+
